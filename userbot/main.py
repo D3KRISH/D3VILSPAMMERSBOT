c46e4b892a5a7c37e9df26842b2668ca6f2b6eb8
Shinchan7222 <83214906+Shinchan7222@users.noreply.github.com>
2021-05-26 15:26:16 +0530

    Update main.py

--- userbot/main.py
+++ userbot/main.py
@@ -1,6 +1,8 @@
-Q# Licensed under the Raphielscape Public License, Version 1.c (the "License");
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
-
+#
 # D3VIL UserBot - D3KRISH
 
 """ UserBot starting point """
